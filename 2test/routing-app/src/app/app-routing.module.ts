468d1c8606ca51bcec825cc49565b863ce92f6cb
George <george_rtaylor@yahoo.com>
2024-07-14 11:31:17 -0500

    commit

---
+++ 2test/routing-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+const routes: Routes = [
+  {
+    path:'first'compnent
+  }
+]
